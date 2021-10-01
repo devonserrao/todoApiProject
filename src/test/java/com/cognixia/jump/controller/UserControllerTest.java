d69896c3a64667f04477ee708ef123398f6feacd
devonserrao <devonserrao101@gmail.com>
2021-10-01 10:56:12 -0500

    Added JUnit test file for UserController and created test for getAllUsers()

---
+++ src/test/java/com/cognixia/jump/controller/UserControllerTest.java
@@ -0,0 +1,56 @@
+package com.cognixia.jump.controller;
+
+import java.util.List;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+
+import com.cognixia.jump.model.ToDo;
+import com.cognixia.jump.model.User;
+
+@ExtendWith(SpringExtension.class)
+@WebMvcTest(UserController.class)
+public class UserControllerTest {
+
+private final String STARTING_URI = "http://localhost:8080/api";
+	
+	@Autowired
+	private MockMvc mockMvc;
+	
+	@MockBean
+	private UserController controller;
+	
+	@Test
+	void testGetAllUsers() throws Exception {
+		
+		String uri = STARTING_URI + "/user";
+		
+		List<User> allUsers = Arrays.asList(
+					new User(1, "Devon Serrao", "devon@gmail.com", "ToastisGood", new ArrayList<ToDo>() ),
+					new User(2, "Darren", "darren@gmail.com", "test123", new ArrayList<ToDo>() ),
+					new User(3, "Testing", "test@gmail.com", "Test12323231", new ArrayList<ToDo>() ),
+					new User(4, "Denver", "denver@gmail.com", "Test12323231", new ArrayList<ToDo>() )
+				);
+		
+		when(controller.getUsers()).thenReturn(allUsers);
+		
+		mockMvc.perform(get(uri))
+				.andDo( print() )
+				.andExpect(status().isOk());
+		
+	}
+	
+}
